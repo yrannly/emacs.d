3b5435fc6277709852029b0e02f6fce700d86e31
yrannly <yrandnight@gmail.com>
2024-02-28 18:49:49 +0800

    1709117389

--- early-init.el
+++ early-init.el
@@ -1,4 +1,117 @@
 (defun tressa/require (file)
   (load (expand-file-name (symbol-name file) user-emacs-directory) t t))
 
-(tressa/require 'builtin.el)
+;; disable-modes
+(line-number-mode -1)
+(menu-bar-mode -1)
+
+;; disable with test
+(if (functionp 'tool-bar-mode)
+    (tool-bar-mode -1))
+
+(defun tressa/var (&optional path)
+  (let ((var (if (getenv "XDG_DATA_HOME")
+                 (concat (getenv "XDG_DATA_HOME") "emacs")
+               (concat user-emacs-directory "var"))))
+    (if path
+	(expand-file-name path var)
+      var)))
+
+;; core
+(setq bidi-inhibit-bpa t
+      create-lockfiles nil
+      gc-cons-threshold (* 100 1024 1024)
+      large-hscroll-threshold 1000
+      locale-coding-system 'utf-8-unix
+      long-line-threshold 1000
+      read-process-output-max (* 1024 1024)
+      ring-bell-function 'ignore
+      use-short-answers t)
+(setq-default bidi-display-reordering nil)
+(set-default-coding-systems 'utf-8-unix)
+(when (eq system-type 'windows-nt)
+  (setq file-name-coding-system 'gbk))
+(let ((dw 1920)
+      (dh 1080)
+      (fw 993)
+      (fh 720))
+  (setq default-frame-alist
+        `((left . ,(- (/ dw 2) (/ fw 2)))
+          (top . ,(- (/ dh 2) (/ fh 2)))
+          (height . 40)
+          (width . 120))))
+(define-fringe-bitmap 'right-curly-arrow
+  [#b00000000
+   #b00000000
+   #b00000000
+   #b00000000
+   #b01110000
+   #b00000000
+   #b00000000
+   #b00000000])
+(define-fringe-bitmap 'left-curly-arrow
+  [#b00000000
+   #b00000000
+   #b00000000
+   #b00001110
+   #b00000000
+   #b00000000
+   #b00000000
+   #b00000000])
+
+;; no-litter
+(setq
+ auto-save-list-file-prefix (tressa/var "auto-save-list/saves-")
+ custom-file (tressa/var "custom.el")
+ desktop-path `(,(tressa/var))
+ org-persist-directory (tressa/var "org-persist")
+ package-user-dir (tressa/var "elpa")
+ recentf-save-file (tressa/var "recentf")
+ savehist-file (tressa/var "history")
+ tramp-auto-save-directory (tressa/var "tramp/auto-save/")
+ tramp-persistency-file-name (tressa/var "tramp/persistency.el")
+ transient-history-file (tressa/var "transient/history.el")
+ transient-levels-file (tressa/var "transient/levels.el")
+ transient-values-file (tressa/var "transient/values.el")
+ treesit-extra-load-path `(,(tressa/var "treesit"))
+ url-configuration-directory (tressa/var "url/"))
+
+;; files
+(setq auto-save-default nil
+      backup-inhibited t
+      make-backup-files nil)
+
+;; package
+(setq package-initialized nil
+      package-archives '(("gnu"    . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/gnu/")
+                         ("nongnu" . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/nongnu/")
+                         ("melpa"  . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/melpa/")))
+
+;; recentf
+(recentf-mode)
+(setq recentf-max-saved-items 100)
+
+;; shell
+(setq explicit-shell-file-name "/bin/bash")
+
+;; simple
+(set-default 'indent-tabs-mode nil)
+
+;; startup
+(setq inhibit-startup-screen t)
+
+;; syntax
+(setq syntax-wholeline-max 1000)
+
+;; vc
+(setq vc-follow-symlinks t)
+
+;; start modes
+(column-number-mode)
+(global-auto-revert-mode)
+(global-display-line-numbers-mode)
+(savehist-mode)
+
+(create-fontset-from-fontset-spec "-*-Cascadia Mono-normal-r-*-*-14-*-*-*-c-*-fontset-custom")
+(set-fontset-font "fontset-custom" 'han (font-spec :family "LXGW Neo Xihei") nil 'prepend)
+(add-to-list 'default-frame-alist '(font . "fontset-custom"))
--- init.el
+++ init.el
@@ -1,2 +1,92 @@
-(tressa/require 'pack.el)
-(tressa/require 'site.el)
+;;; pack
+(require 'use-package-ensure)
+(setq use-package-always-defer t
+      use-package-always-ensure t)
+
+(use-package avy
+  :bind
+  (:map mode-specific-map
+   ("a" . avy-goto-char)))
+
+(use-package company
+  :custom
+  (company-dabbrev-downcase nil)
+  (company-minimum-prefix-length 1)
+  :init
+  (global-company-mode))
+
+(use-package consult
+  :bind
+  (("C-s" . consult-line)
+   :map mode-specific-map
+   ("cd" . consult-flymake)
+   ("cf" . consult-find)
+   ("cg" . consult-ripgrep)))
+
+(use-package expand-region
+  :bind
+  (:map mode-specific-map
+   ("e'" . er/mark-inside-quotes)
+   ("ea'" . er/mark-outside-quotes)
+   ("e]" . er/mark-inside-pairs)
+   ("ea]" . er/mark-outside-pairs)))
+
+(use-package flymake-eslint)
+
+(use-package marginalia
+  :init
+  (marginalia-mode))
+
+(use-package orderless
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides '((file (styles partial-completion)))))
+
+(use-package treesit-auto
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode)
+  :init
+  (require 'treesit-auto))
+
+(use-package vertico
+  :init
+  (vertico-mode))
+
+(use-package web-mode
+  :custom
+  (web-mode-code-indent-offset 2)
+  (web-mode-css-indent-offset 2)
+  (web-mode-enable-auto-closing t)
+  (web-mode-markup-indent-offset 2)
+  :init
+  (define-derived-mode vue-mode web-mode "Vue")
+  (add-to-list 'auto-mode-alist '("\\.vue\\'" . vue-mode)))
+
+(use-package wgrep)
+
+;;; site
+(keymap-global-unset "C-@")
+(keymap-global-unset "C-M-\\")
+(keymap-global-unset "C-SPC")
+(keymap-global-unset "C-x C-;")
+(keymap-set mode-specific-map "f" 'indent-region)
+(keymap-set mode-specific-map "r" 'recentf-open)
+(keymap-set mode-specific-map "s" 'set-mark-command)
+(keymap-set mode-specific-map "w b" 'windmove-left)
+(keymap-set mode-specific-map "w f" 'windmove-right)
+(keymap-set mode-specific-map "w n" 'windmove-down)
+(keymap-set mode-specific-map "w p" 'windmove-up)
+
+(when (string-match "WSL2" operating-system-release)
+  ;; browse
+  (setq browse-url-chrome-program "/mnt/c/Program Files (x86)/Microsoft/Edge/Application/msedge.exe"
+        browse-url-browser-function 'browse-url-chrome)
+  ;; clipboard
+  (setq interprogram-cut-function #'(lambda (text)
+                                      (let* ((process-connection-type nil)
+                                             (proc (start-process "xclip" nil
+                                                                  "/mnt/c/Windows/System32/clip.exe")))
+                                        (process-send-string proc text)
+                                        (process-send-eof proc)))))
